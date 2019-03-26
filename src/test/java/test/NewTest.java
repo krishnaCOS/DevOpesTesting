783312c8944707bbaf2ca28112c59c49769f09c2
Kaparthi <k.kaparthi@accenture.com>
2019-03-26 15:05:21 +0530

    MavenFiles

--- src/test/java/test/NewTest.java
+++ src/test/java/test/NewTest.java
@@ -1,10 +1,25 @@
 package test;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 
 public class NewTest {
   @Test
   public void f() {
 	  System.out.print("test proj");
+	  System.setProperty("webdriver.chrome.driver","C:\\Users\\k.kaparthi\\Downloads\\Training\\chromedriver.exe");
+	  //launch chrome browser
+	  WebDriver driver=new ChromeDriver();
+	  //open the application
+	  driver.get("http://newtours.demoaut.com/");
+	  
+	  //enter username
+	  driver.findElement(By.name("userName")).sendKeys("mercury");
+	  driver.findElement(By.name("password")).sendKeys("mercury");
+	  driver.findElement(By.name("login")).click();
+	  driver.close();
+
   }
 }
